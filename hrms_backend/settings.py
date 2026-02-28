5e9f2a6a037c71735c455c162f31726b80a499b0
root29 <vatsakash168@gmail.com>
2026-02-28 14:25:54 +0530

    feat: Initialize Django project settings, configuring environment variable loading, database, JWT, and CORS.

--- hrms_backend/settings.py
+++ hrms_backend/settings.py
@@ -71,11 +71,11 @@ if os.getenv('DB_ENGINE') == 'postgres':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT', '5432'),
+            'NAME': os.getenv('PGDATABASE'),
+            'USER': os.getenv('PGUSER'),
+            'PASSWORD': os.getenv('PGPASSWORD'),
+            'HOST': os.getenv('PGHOST'),
+            'PORT': os.getenv('PGPORT', '5432'),
         }
     }
 else:
